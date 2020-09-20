93c2c7e270675847b7c14a420d42d0f86d7783e3
uncleejay <joseph.c.o216@gmail.com>
2020-09-20 14:32:59 +0100

    partly done. shows lists of blog posts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import { createStore, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import rootReducer from './reducers/index'
+import { Navbar} from './components/Navbar'
+import DashboardPage from './pages/DashboardPage'
+import PostsPage from './pages/PostsPage'
+import SinglePostPage from './pages/SinglePostPage'
+
+// create store
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+const Routing = () => {
+    return(
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route exact path="/" component={DashboardPage}/>
+          <Route exact path="/posts" component={PostsPage}/>
+          <Route exact path="/posts/:id" component={SinglePostPage}/>
+          <Redirect to="/"/>
+        </Switch>
+      </Router>
+    )
+}
+
+const App = () => {
+  return(
+      <Provider store={store}>
+          <Routing/>
+      </Provider>
   );
 }
 
---
+++ src/pages/SinglePostPage.js
@@ -0,0 +1,7 @@
+import React, {useEffect} from 'react'
+
+const SinglePostPage = () => {
+
+}
+
+export default SinglePostPage;
